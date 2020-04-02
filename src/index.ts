26050107c25b77c7ab9eb3fe498938ad97c19341
Safraz Razik <safrazik@gmail.com>
2020-04-02 17:08:01 +0530

    Export Utils

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { Builder } from './Builder';
 export { Parser } from './Parser';
 export { Rockplate } from './Rockplate';
 export { Linter, Lint, LinterResult } from './Linter';
+export { Utils } from './Utils';
 export * from './block/index';
